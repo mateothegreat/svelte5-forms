fd80e222b57b59b3fda4ad477095751732ae4f77
Matthew Davis <matthew@matthewdavis.io>
2024-10-25 14:03:12 -0500

    bump

--- src/lib/controls/switch.svelte
+++ src/lib/controls/switch.svelte
@@ -37,11 +37,9 @@
     {@render children()}
   </Label.Root>
   <Switch.Root
-    class="peer inline-flex h-5 w-10 cursor-pointer items-center rounded-full bg-gray-600 px-0.5 transition-colors focus-visible:ring-2 focus-visible:ring-green-500
-           focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-green-500 data-[state=unchecked]:bg-gray-300">
+    class="peer inline-flex h-5 w-10 cursor-pointer items-center rounded-full bg-gray-600 px-0.5 transition-colors focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-green-500 data-[state=unchecked]:bg-gray-300">
     <Switch.Thumb
-      class="pointer-events-none block h-4 w-4 transform rounded-full bg-black shadow transition-transform
-             data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0" />
+      class="pointer-events-none block h-4 w-4 transform rounded-full bg-black shadow transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0" />
   </Switch.Root>
 </div>
 
